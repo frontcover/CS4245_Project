1edfb139b0bb1649b75ab71866dee90803d02356
Simin Zhu <szhu2@login02.delftblue.tudelft.net>
2022-05-26 00:21:08 +0200

    your message

--- Slurm/sub_data_transfer.sh
+++ Slurm/sub_data_transfer.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+!/bin/sh
 #
-#SBATCH --job-name="js_data"
+#SBATCH --job-name="Move_data"
 #SBATCH --partition=trans
 #SBATCH --time=01:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 
-rsync -av /tudelft.net/staff-groups/ewi/me/MS3/Studenten/'Simin Zhu'/SLAM/Dataset /scratch/szhu2/ 
+rsync -a /tudelft.net/staff-groups/ewi/me/MS3/Studenten/'Simin Zhu'/SLAM/Dataset /scratch/szhu2/
+
