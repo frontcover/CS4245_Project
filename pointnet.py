b90d2600a44f38b875609887fc779ca54ae70402
celmoussaoui <C.ElMoussaoui@student.tudelft.nl>
2022-06-04 11:07:35 +0200

    Load in data as numpy array for pointnet

--- pointnet.py
+++ pointnet.py
@@ -1,6 +1,5 @@
-import csv
 import os
-import trimesh
+import numpy as np
 
 if __name__ == "__main__":
     for i in os.listdir('MATLAB/Point Cloud Dataset'):
@@ -8,21 +7,5 @@ if __name__ == "__main__":
         for j in os.listdir('MATLAB/Point Cloud Dataset/' + i):
             print('File:', j)
             file = open('MATLAB/Point Cloud Dataset/' + i + '/' + j)
-            tri = trimesh.load('MATLAB/Point Cloud Dataset/' + i + '/' + j)
-            print(tri)
-
-            # Convert csv into python arrays
-            # csv_reader = csv.reader(file)
-            # rows = []
-            # for row in csv_reader:
-            #     rows.append(row)
-            #
-            # point_set = []
-            # for index, data_point in enumerate(rows[0]):
-            #     point = [data_point, rows[1][index], rows[2][index], rows[3][index]]
-            #     point_set.append(point)
-            #     s = trimesh.Trimesh(point)
-            #     print(type(s))
-            #     print(s)
-            #     break
-            # print(point_set)
+            data = np.genfromtxt('MATLAB/Point Cloud Dataset/' + i + '/' + j, delimiter=',')
+            print(data)
