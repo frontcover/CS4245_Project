a6307b43056b1828dace398b2ed157bccf52a72b
Simmy <70203356+SimmyZhu@users.noreply.github.com>
2022-06-05 14:28:53 +0200

    add BN changes

--- utils.py
+++ utils.py
@@ -5,23 +5,23 @@
 # Link: https://colab.research.google.com/github/keras-team/keras-io/blob/master/examples/vision/ipynb/pointnet.ipynb#scrollTo=fNjnhuBSXCs0
 #############################################################################
 import tensorflow as tf
-from tensorflow.python import keras
 from tensorflow.python.keras import layers
 import numpy as np
 
+
 def conv_bn(x, filters):
     x = layers.Conv1D(filters, kernel_size=1, padding="valid")(x)
-    x = layers.BatchNormalization(momentum=0.0)(x)
+    x = tf.keras.layers.BatchNormalization(momentum=0.0)(x)
     return layers.Activation("relu")(x)
 
 
 def dense_bn(x, filters):
     x = layers.Dense(filters)(x)
-    x = layers.BatchNormalization(momentum=0.0)(x)
+    x = tf.keras.layers.BatchNormalization(momentum=0.0)(x)
     return layers.Activation("relu")(x)
 
 
-class OrthogonalRegularizer(keras.regularizers.Regularizer):
+class OrthogonalRegularizer(tf.keras.regularizers.Regularizer):
     def __init__(self, num_features, l2reg=0.001):
         self.num_features = num_features
         self.l2reg = l2reg
@@ -36,7 +36,7 @@ class OrthogonalRegularizer(keras.regularizers.Regularizer):
 
 def tnet(inputs, num_features):
     # Initalise bias as the indentity matrix
-    bias = keras.initializers.Constant(np.eye(num_features).flatten())
+    bias = tf.keras.initializers.Constant(np.eye(num_features).flatten())
     reg = OrthogonalRegularizer(num_features)
 
     x = conv_bn(inputs, 32)
@@ -51,7 +51,7 @@ def tnet(inputs, num_features):
         bias_initializer=bias,
         activity_regularizer=reg,
     )(x)
-    feat_T = layers.Reshape((num_features, num_features))(x)
+    feat_t = layers.Reshape((num_features, num_features))(x)
 
     # Apply affine transformation to input features
-    return layers.Dot(axes=(2, 1))([inputs, feat_T])
+    return layers.Dot(axes=(2, 1))([inputs, feat_t])
